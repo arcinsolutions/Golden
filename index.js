0342e69d0a95e0c3dcb94ced2650b3e2d5e3af3d
BotisDerG <barnekiel@gmail.com>
2021-09-02 14:37:17 +0200

    init v0.25

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-
+/** ++ Discord ++ **/
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config/config.json');
+//const { config } = require('./config/config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+/** -- Discord -- **/
+
+/** ++ Color & Console Log+Start ++ **/
+var colors = require('colors/safe');
 
 client.once('ready', () => {
-	console.log('Ready!');
+
+	const text =  client.user.tag + ' is Online!';
+	var underline = "";
+
+	for (var i=0; i<= text.length-1; i++ ) {
+		underline += "▬";
+	}
+
+	console.log(colors.green(underline));
+	console.log(colors.green.bold(text));
+	console.log(colors.green(underline));
 });
 
-client.login(token);
+client.login('ODY0OTcxNDExMDY3NjMzNjY0.YO9NZg.EU11g0ob7ipC4MkDEKV8F_1Z1zo');
+/** -- Color & Console Log -- **/
