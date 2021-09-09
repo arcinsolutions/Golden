2f74a781155a1707fbe31dfa5693652713f6d7e2
BotisDerG <barnekiel@gmail.com>
2021-09-09 08:10:01 +0200

    Stats | NOT DONE!

--- events/ready.js
+++ events/ready.js
@@ -6,13 +6,19 @@ module.exports = {
     execute(client) {
             const text =  client.user.tag + ' is Online!';
             var underline = "";
+
+            const stats = [`${client.guilds}`, ``]
         
             for (var i=1; i<= text.length; i++ ) {
-                underline += "▬";
+                underline += "━";
             }
         
             console.log(colors.green(underline));
             console.log(colors.green.bold(text));
             console.log(colors.green(underline));
+
+            console.log('┏' + "━".repeat(-2+text.length) + '┓');
+            console.log('┃ Stats:' + " ".repeat(-9+text.length) + '┃');
+            console.log('┃ Guilds:' + stats[1] + " ".repeat(-10+text.length-stats[1].length) + '┃');
         }
     }
