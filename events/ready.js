3dbf5e6d8b76da841028dfeb4f3dbe3d6400d47b
BotisDerG <barnekiel@gmail.com>
2021-09-10 08:15:26 +0200

    Stats at Startup | NOT DONE!

--- events/ready.js
+++ events/ready.js
@@ -4,21 +4,25 @@ module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
-            const text =  client.user.tag + ' is Online!';
+            const text =  `${client.user.tag} is Online!`;
             var underline = "";
 
-            const stats = [`${client.guilds}`, ``]
-        
+            const stats = [];
+            stats[0] = ` ${client.guilds.cache.size}`;
+            stats[1] = ` ${client.users.cache.size}`;
             for (var i=1; i<= text.length; i++ ) {
                 underline += "━";
             }
-        
+            
+            console.log(stats[1])
+
             console.log(colors.green(underline));
             console.log(colors.green.bold(text));
             console.log(colors.green(underline));
 
             console.log('┏' + "━".repeat(-2+text.length) + '┓');
-            console.log('┃ Stats:' + " ".repeat(-9+text.length) + '┃');
-            console.log('┃ Guilds:' + stats[1] + " ".repeat(-10+text.length-stats[1].length) + '┃');
+            console.log('┃ ' + colors.bold.underline('Stats:') + " ".repeat(-9+text.length) + '┃');
+            console.log('┃  Guilds:' + stats[0] + ' '.repeat(text.length-stats[0].length-11) + '┃');
+            console.log('┃  Users:' + stats[1] + " ".repeat(text.length-stats[0].length-10) + '┃');
         }
     }
