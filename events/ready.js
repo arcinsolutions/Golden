054e5deb09fb6f938a8cf6f1b86ed1d27c2ba9d9
BotisDerG <barnekiel@gmail.com>
2021-09-10 13:41:30 +0200

    Stats at startup done

--- events/ready.js
+++ events/ready.js
@@ -10,19 +10,21 @@ module.exports = {
             const stats = [];
             stats[0] = ` ${client.guilds.cache.size}`;
             stats[1] = ` ${client.users.cache.size}`;
+            stats[2] = ` ${client.channels.cache.size}`
             for (var i=1; i<= text.length; i++ ) {
-                underline += "━";
+                underline += "═";
             }
             
-            console.log(stats[1])
+            console.log(colors.green('╔' + underline + '╗'));
+            console.log('' + colors.green('║' + colors.red.bold(text) + '║'));
+            console.log(colors.green('╠' + underline + '╣'));
 
-            console.log(colors.green(underline));
-            console.log(colors.green.bold(text));
-            console.log(colors.green(underline));
+            console.log(colors.green('║ ') + colors.bold.underline('Stats:') + " ".repeat(-7+text.length) + colors.green('║ '));
+            console.log(colors.green('║ ') + ' Guilds:   ' + stats[0] + ' '.repeat(text.length-stats[0].length-12) + colors.green('║ '));
+            console.log(colors.green('║ ') + ' Users:    ' + stats[1] + " ".repeat(text.length-stats[1].length-12) + colors.green('║ '));
+            console.log(colors.green('║ ') + ' Channels: ' + stats[2] + " ".repeat(text.length-stats[2].length-12) + colors.green('║ '));
 
-            console.log('┏' + "━".repeat(-2+text.length) + '┓');
-            console.log('┃ ' + colors.bold.underline('Stats:') + " ".repeat(-9+text.length) + '┃');
-            console.log('┃  Guilds:' + stats[0] + ' '.repeat(text.length-stats[0].length-11) + '┃');
-            console.log('┃  Users:' + stats[1] + " ".repeat(text.length-stats[0].length-10) + '┃');
+
+            console.log(colors.green('╚' + underline + '╝'));
         }
     }
