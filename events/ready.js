0f95ab51348e9b137afd7c0d0f291b8136ab15cd
= <=>
2021-09-10 23:40:56 +0200

    Fixed Event handler + added to Stats at startup

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 var colors = require('colors/safe');
+const fs = require('fs');
 
 module.exports = {
     name: 'ready',
@@ -10,7 +11,7 @@ module.exports = {
   ██║  ███╗██║   ██║██║     ██║  ██║█████╗  ██╔██╗ ██║ 
   ██║   ██║██║   ██║██║     ██║  ██║██╔══╝  ██║╚██╗██║ 
   ╚██████╔╝╚██████╔╝███████╗██████╔╝███████╗██║ ╚████║ 
-   ╚═════╝  ╚═════╝ ╚══════╝╚═════╝ ╚══════╝╚═╝  ╚═══╝     v0.25
+   ╚═════╝  ╚═════╝ ╚══════╝╚═════╝ ╚══════╝╚═╝  ╚═══╝     v0.5
             ██╗  ██╗ █████╗ ███╗   ██╗██████╗ ██╗     ███████╗██████╗
             ██║  ██║██╔══██╗████╗  ██║██╔══██╗██║     ██╔════╝██╔══██╗
             ███████║███████║██╔██╗ ██║██║  ██║██║     █████╗  ██████╔╝
@@ -19,27 +20,44 @@ module.exports = {
             ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝
             `;
             const text =  `  ${client.user.tag} is Online!  `;
-            var underline = "";
+            var underline = [];
+            underline[0] = "";
+            underline[1] = "";
+
+
+            var files = [];
+
+            fs.readdirSync('./commands/').forEach((dir) => {
+                files[0] = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
+            })
+
+            files[1] = fs.readdirSync(`./events/`).filter(file => file.endsWith('.js'));
 
             const stats = [];
             stats[0] = ` ${client.guilds.cache.size}`;
             stats[1] = ` ${client.users.cache.size}`;
             stats[2] = ` ${client.channels.cache.size}`
+            stats[3] = ` ${files[0].length}`
+            stats[4] = ` ${files[1].length}`
             for (var i=1; i<= text.length; i++ ) {
-                underline += "═";
+                underline[0] += "═";
+                underline[1] += "╴";
             }
             
             console.log(colors.red(imgText))
 
-            console.log(colors.green(`            ╔${underline}╗`));
+            console.log(colors.green(`            ╔${underline[0]}╗`));
             console.log('' + colors.green(`            ║${colors.red.bold(text)}║`));
-            console.log(colors.green('            ╠' + underline + '╣'));
+            console.log(colors.green('            ╠' + underline[0] + '╣'));
 
             console.log(colors.green('            ║ ') + colors.bold.underline('Stats:') + " ".repeat(-7+text.length) + colors.green('║ '));
             console.log(colors.green('            ║ ') + ' Guilds:   ' + stats[0] + ' '.repeat(text.length-stats[0].length-12) + colors.green('║ '));
             console.log(colors.green('            ║ ') + ' Users:    ' + stats[1] + " ".repeat(text.length-stats[1].length-12) + colors.green('║ '));
             console.log(colors.green('            ║ ') + ' Channels: ' + stats[2] + " ".repeat(text.length-stats[2].length-12) + colors.green('║ '));
-            console.log(colors.green('            ╚' + underline + '╝'));
+            console.log(colors.green('            ╟' + underline[1] + '╢'));
+            console.log(colors.green('            ║ ') + ' Commands: ' + stats[3] + " ".repeat(text.length-stats[3].length-12) + colors.green('║ '));
+            console.log(colors.green('            ║ ') + ' Events:   ' + stats[4] + " ".repeat(text.length-stats[4].length-12) + colors.green('║ '));
+            console.log(colors.green('            ╚' + underline[0] + '╝'));
         }
     }
 
--- index.js
+++ index.js
@@ -9,13 +9,17 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 /** ++ Command Handler ++ */
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+var commandFiles = "";
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+fs.readdirSync('./commands/').forEach((dir) => {
+	commandFiles = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`./commands/${dir}/${file}`);
+	
+		client.commands.set(command.data.name, command);
+	}
+})
 
-	client.commands.set(command.data.name, command);
-}
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
@@ -34,18 +38,18 @@ client.on('interactionCreate', async interaction => {
 /** -- Command Handler -- */
 
 /** ++ Event Handler ++ */
+var eventFiles;
 
-const eventFiles = fs.readdirSync(`./events`).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-
-	if (event.once) {
-		client.once(event.name, (...args)=> event.execute(...args));
-	} else {
-		client.on(event.name, (...args)=> event.execute(...args));
+	eventFiles = fs.readdirSync(`./events/`).filter(file => file.endsWith('.js'));
+	for (const file of eventFiles) {
+		const event = require(`./events/${file}`);
+	
+		if (event.once) {
+			client.once(event.name, (...args)=> event.execute(...args));
+		} else {
+			client.on(event.name, (...args)=> event.execute(...args));
+		}
 	}
-}
 
 /** -- Event Handler -- */
 
