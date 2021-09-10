9b81b1eb6f8e8554b8db3211f3de5cd8592cb4c8
BotisDerG <barnekiel@gmail.com>
2021-09-10 14:16:50 +0200

    added Ad xD

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,14 @@ module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
+
+            const imgText = [];
+            imgText[0] = ' _____       _     _              _   _                 _ _           ';
+            imgText[1] = '|  __ \\     | |   | |            | | | |               | | |          ';
+            imgText[2] = '| |  \\/ ___ | | __| | ___ _ __   | |_| | __ _ _ __   __| | | ___ _ __ ';
+            imgText[3] = `| | __ / _ \\| |/ _' |/ _ \\ '_ \\  |  _  |/ _' | '_ \\ / _' | |/ _ \\ '__|`;
+            imgText[4] = `| |_\\ \\ (_) | | (_| |  __/ | | | | | | | (_| | | | | (_| | |  __/ |   `;
+            imgText[5] = `\\_____/\\___/|_|\\__,_|\\___|_| |_| \\_| |_/\\__,_|_| |_|\\__,_|_|\\___|_|`;
             const text =  `${client.user.tag} is Online!`;
             var underline = "";
 
@@ -15,6 +23,10 @@ module.exports = {
                 underline += "═";
             }
             
+            for ( i = 0; i < imgText.length; i++) {
+                console.log( colors.red(imgText[i]));
+            }
+
             console.log(colors.green('╔' + underline + '╗'));
             console.log('' + colors.green('║' + colors.red.bold(text) + '║'));
             console.log(colors.green('╠' + underline + '╣'));
@@ -23,8 +35,6 @@ module.exports = {
             console.log(colors.green('║ ') + ' Guilds:   ' + stats[0] + ' '.repeat(text.length-stats[0].length-12) + colors.green('║ '));
             console.log(colors.green('║ ') + ' Users:    ' + stats[1] + " ".repeat(text.length-stats[1].length-12) + colors.green('║ '));
             console.log(colors.green('║ ') + ' Channels: ' + stats[2] + " ".repeat(text.length-stats[2].length-12) + colors.green('║ '));
-
-
             console.log(colors.green('╚' + underline + '╝'));
         }
     }
