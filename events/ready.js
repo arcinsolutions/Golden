7974d6a7deb71a4e21fb4f8e88f0d54a7e322f60
= <=>
2021-09-11 18:51:36 +0200

    small change for better code

--- events/ready.js
+++ events/ready.js
@@ -27,23 +27,19 @@ module.exports = {
 
             var files = [];
 
-            fs.readdirSync('./commands/').forEach((dir) => {
-                files[0] = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
-            })
-
             files[1] = fs.readdirSync(`./events/`).filter(file => file.endsWith('.js'));
 
             const stats = [];
             stats[0] = ` ${client.guilds.cache.size}`;
             stats[1] = ` ${client.users.cache.size}`;
             stats[2] = ` ${client.channels.cache.size}`
-            stats[3] = ` ${files[0].length}`
+            stats[3] = ` ${client.commands.size}`
             stats[4] = ` ${files[1].length}`
             for (var i=1; i<= text.length; i++ ) {
                 underline[0] += "═";
                 underline[1] += "╴";
             }
-            
+                        
             console.log(colors.red(imgText))
 
             console.log(colors.green(`            ╔${underline[0]}╗`));
