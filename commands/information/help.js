1a7312813290862f5fc5406d6983e98dfa723bd2
= <=>
2021-09-12 21:15:28 +0200

    Prettier Code + Help Menu | NOT DONE!

--- commands/information/help.js
+++ commands/information/help.js
@@ -1,10 +1,14 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('@discordjs/builders')
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('help')
-		.setDescription('Replies with Help!'),
-	async execute(interaction) {
-		await interaction.reply('Help!');
-	},
-};
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Replies with a nice Menu!'),
+  async execute (interaction, client) {
+    const commandsCollection = client.commands.map(description => {
+      return `${description.data.name} - ${description.data.description}\n` // Hide syntax if empty
+    })
+
+    await interaction.reply(commandsCollection.join(''))
+  }
+}
