fa81587da6c24a389da4e73c22e8328c1d30b6d5
= <=>
2021-09-12 21:18:25 +0200

    Added Activity (changes every minute) + Prettier..

--- events/ready.js
+++ events/ready.js
@@ -1,25 +1,14 @@
 var colors = require('colors/safe');
 const fs = require('fs');
+const { getRandomNumber } = require('../functions/random');
+
+import('../functions/random.js')
 
 module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
-            const imgText = `
-   ██████╗  ██████╗ ██╗     ██████╗ ███████╗███╗   ██╗ 
-  ██╔════╝ ██╔═══██╗██║     ██╔══██╗██╔════╝████╗  ██║ 
-  ██║  ███╗██║   ██║██║     ██║  ██║█████╗  ██╔██╗ ██║ 
-  ██║   ██║██║   ██║██║     ██║  ██║██╔══╝  ██║╚██╗██║ 
-  ╚██████╔╝╚██████╔╝███████╗██████╔╝███████╗██║ ╚████║ 
-   ╚═════╝  ╚═════╝ ╚══════╝╚═════╝ ╚══════╝╚═╝  ╚═══╝     v0.5
-            ██╗  ██╗ █████╗ ███╗   ██╗██████╗ ██╗     ███████╗██████╗
-            ██║  ██║██╔══██╗████╗  ██║██╔══██╗██║     ██╔════╝██╔══██╗
-            ███████║███████║██╔██╗ ██║██║  ██║██║     █████╗  ██████╔╝
-            ██╔══██║██╔══██║██║╚██╗██║██║  ██║██║     ██╔══╝  ██╔══██╗
-            ██║  ██║██║  ██║██║ ╚████║██████╔╝███████╗███████╗██║  ██║
-            ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝
-            `;
-            const text =  `  ${client.user.tag} is Online!  `;
+            const text =  `  ${client.user.tag} is Online now!  `;
             var underline = [];
             underline[0] = "";
             underline[1] = "";
@@ -39,21 +28,57 @@ module.exports = {
                 underline[0] += "═";
                 underline[1] += "╴";
             }
-                        
-            console.log(colors.red(imgText))
-
+            
             console.log(colors.green(`            ╔${underline[0]}╗`));
             console.log('' + colors.green(`            ║${colors.red.bold(text)}║`));
             console.log(colors.green('            ╠' + underline[0] + '╣'));
 
             console.log(colors.green('            ║ ') + colors.bold.underline('Stats:') + " ".repeat(-7+text.length) + colors.green('║ '));
             console.log(colors.green('            ║ ') + ' Guilds:   ' + stats[0] + ' '.repeat(text.length-stats[0].length-12) + colors.green('║ '));
-            console.log(colors.green('            ║ ') + ' Users:    ' + stats[1] + " ".repeat(text.length-stats[1].length-12) + colors.green('║ '));
+            console.log(colors.green('            ║ ') + ' User:     ' + stats[1] + " ".repeat(text.length-stats[1].length-12) + colors.green('║ '));
             console.log(colors.green('            ║ ') + ' Channels: ' + stats[2] + " ".repeat(text.length-stats[2].length-12) + colors.green('║ '));
             console.log(colors.green('            ╟' + underline[1] + '╢'));
             console.log(colors.green('            ║ ') + ' Commands: ' + stats[3] + " ".repeat(text.length-stats[3].length-12) + colors.green('║ '));
             console.log(colors.green('            ║ ') + ' Events:   ' + stats[4] + " ".repeat(text.length-stats[4].length-12) + colors.green('║ '));
             console.log(colors.green('            ╚' + underline[0] + '╝'));
+
+
+            //** ++ Activity ++ */ 
+
+            var currentActivity = getRandomNumber(1, 4);
+
+            setInterval(()=>{
+                try{
+                    switch(currentActivity) {
+                        case 1:
+                            client.user.setActivity(`${stats[0]} Guilds`, { type: 'LISTENING' });
+                            currentActivity = getRandomNumber(0, 5);
+                            break;
+                        case 2:
+                            client.user.setActivity(`${stats[1]} Members`, { type: 'LISTENING' });
+                            currentActivity = getRandomNumber(0, 5);
+                            break;
+                        case 3:
+                            client.user.setActivity(`${stats[2]} Channels`, { type: 'LISTENING' });
+                            currentActivity = getRandomNumber(0, 5);
+                            break;
+                        case 4:
+                            client.user.setActivity(`Asking @Spastencord#1289 for Games`, { type: 'CUSTOM' });
+                            currentActivity = getRandomNumber(0, 5);
+                            break;
+                        default:
+                            client.user.setActivity(`@Botis‽#6940`, { type: 'WATCHING' });
+                            currentActivity = getRandomNumber(0, 5);
+                            break;
+                    }
+                }catch (e) {
+                  console.log(e);
+                }
+              }, 10*60*100)
+
+            client.user.setActivity(`${stats[0]} Guilds`, { type: 'LISTENING' });
+            
+            //** -- Activity -- */
         }
     }
 
