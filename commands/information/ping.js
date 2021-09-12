76767853b2888014cf3dbdeb9e5c0d9a8344b90f
= <=>
2021-09-12 21:20:16 +0200

    Prettier Code

--- commands/information/ping.js
+++ commands/information/ping.js
@@ -1,11 +1,17 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('@discordjs/builders')
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
-		interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
-	},
-};
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!'),
+  async execute (interaction) {
+    const sent = await interaction.reply({
+      content: 'Pinging...',
+      fetchReply: true
+    })
+    interaction.editReply(
+      `Roundtrip latency: ${sent.createdTimestamp -
+        interaction.createdTimestamp}ms`
+    )
+  }
+}
--- index.js
+++ index.js
@@ -1,61 +1,107 @@
 /** ++ Discord init ++ **/
-const { Client, Collection, Intents } = require('discord.js');
-const fs = require('fs');
-///const { config } = require('./config/config.json');
+const { Client, Collection, Intents } = require('discord.js')
+const fs = require('fs')
 
-require('dotenv').config({ path: './config/.env'});
+require('dotenv').config({ path: './config/.env' })
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-/** ++ Command Handler ++ */
-client.commands = new Collection();
-var commandFiles = "";
-
-fs.readdirSync('./commands/').forEach((dir) => {
-	commandFiles = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const command = require(`./commands/${dir}/${file}`);
-	
-		client.commands.set(command.data.name, command);
-	}
+const client = new Client({
+  fetchAllMembers: true,
+  // restTimeOffset: 0,
+  // restWsBridgetimeout: 100,
+  shards: 'auto',
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER', 'GUILD_MEMBER'],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_BANS,
+    // Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_INTEGRATIONS,
+    // Intents.FLAGS.GUILD_WEBHOOKS,
+    // Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING
+  ]
 })
 
+/** ++ Command Handler ++ */
+
+client.commands = new Collection()
+client.categories = require('fs').readdirSync(`./commands`)
+var commandFiles = ''
+
+fs.readdirSync('./commands/').forEach(dir => {
+  commandFiles = fs
+    .readdirSync(`./commands/${dir}/`)
+    .filter(file => file.endsWith('.js'))
+  for (const file of commandFiles) {
+    const command = require(`./commands/${dir}/${file}`)
+
+    client.commands.set(command.data.name, command)
+  }
+})
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return
 
-	const command = client.commands.get(interaction.commandName);
+  const command = client.commands.get(interaction.commandName)
 
-	if (!command) return interaction.reply('please submit a Valid command!');
+  if (!command) return interaction.reply('please submit a Valid command!')
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
+  try {
+    await command.execute(interaction, client)
+  } catch (error) {
+    console.error(error)
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true
+    })
+  }
+})
+
 /** -- Command Handler -- */
 
 /** ++ Event Handler ++ */
-var eventFiles;
 
-	eventFiles = fs.readdirSync(`./events/`).filter(file => file.endsWith('.js'));
-	for (const file of eventFiles) {
-		const event = require(`./events/${file}`);
-	
-		if (event.once) {
-			client.once(event.name, (...args)=> event.execute(...args));
-		} else {
-			client.on(event.name, (...args)=> event.execute(...args));
-		}
-	}
+var eventFiles
+
+eventFiles = fs.readdirSync(`./events/`).filter(file => file.endsWith('.js'))
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`)
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args))
+  } else {
+    client.on(event.name, (...args) => event.execute(...args))
+  }
+}
 
 /** -- Event Handler -- */
-
-
 /** -- Discord init -- **/
 
 /** ++ Start ++ **/
-client.login(process.env.TOKEN);
+
+var colors = require('colors/safe')
+
+const imgText = `
+ ██████╗  ██████╗ ██╗     ██████╗ ███████╗███╗   ██╗ 
+██╔════╝ ██╔═══██╗██║     ██╔══██╗██╔════╝████╗  ██║ 
+██║  ███╗██║   ██║██║     ██║  ██║█████╗  ██╔██╗ ██║ 
+██║   ██║██║   ██║██║     ██║  ██║██╔══╝  ██║╚██╗██║ 
+╚██████╔╝╚██████╔╝███████╗██████╔╝███████╗██║ ╚████║ 
+ ╚═════╝  ╚═════╝ ╚══════╝╚═════╝ ╚══════╝╚═╝  ╚═══╝     v0.75
+         ██╗  ██╗ █████╗ ███╗   ██╗██████╗ ██╗     ███████╗██████╗
+         ██║  ██║██╔══██╗████╗  ██║██╔══██╗██║     ██╔════╝██╔══██╗
+         ███████║███████║██╔██╗ ██║██║  ██║██║     █████╗  ██████╔╝
+         ██╔══██║██╔══██║██║╚██╗██║██║  ██║██║     ██╔══╝  ██╔══██╗
+         ██║  ██║██║  ██║██║ ╚████║██████╔╝███████╗███████╗██║  ██║
+         ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝
+         `
+
+console.log(colors.red(imgText))
+
+client.login(process.env.TOKEN)
+
 /** -- Start -- **/
