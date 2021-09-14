d2a0f8019ae23395a6861f1d74cf93c4e57351ff
= <=>
2021-09-14 20:47:02 +0200

    Added Embed

--- commands/information/ping.js
+++ commands/information/ping.js
@@ -1,17 +1,32 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!'),
-  async execute (interaction) {
+  async execute (interaction, client) {
+    let embed = new MessageEmbed()
+      .setColor('GREEN')
+      .setFooter(client.user.username, client.user.displayAvatarURL())
+      .setTimestamp()
+
     const sent = await interaction.reply({
-      content: 'Pinging...',
+      embeds: [
+        embed.setDescription(
+          `**Pinging...**`
+        )
+      ],
       fetchReply: true
     })
-    interaction.editReply(
-      `Roundtrip latency: ${sent.createdTimestamp -
-        interaction.createdTimestamp}ms`
-    )
+
+    interaction.editReply({
+      embeds: [
+        embed.setDescription(
+          `**Roundtrip latency: ${sent.createdTimestamp -
+            interaction.createdTimestamp}ms**`
+        )
+      ]
+    })
   }
 }
