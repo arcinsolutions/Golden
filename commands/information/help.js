dda0cb312cde9dcbe509be02e37eed5d42e91da5
Barne Bellin <barnekiel@gmail.com>
2021-10-20 10:38:15 +0200

    Added Help Menu

--- commands/information/help.js
+++ commands/information/help.js
@@ -1,47 +1,32 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
+
 module.exports = {
 	data: new SlashCommandBuilder().setName('help').setDescription('Replies with a nice Menu!'),
 	async execute(interaction, client) {
-		const commandsNames = client.commands.map((description) => {
-			return `${description.data.name}`; // Hide syntax if empty
-		});
-		const commandsNamesArr = Array.from(commandsNames);
 
-		const commandsDescriptions = client.commands.map((description) => {
-			return `${description.data.description}`; // Hide syntax if empty
-		});
-		const commandsDescriptionsArr = Array.from(commandsDescriptions);
+		const commandsNames = client.commands.map((description) => {
+			let temp = { "Name": description.data.name, "Description": description.data.description};
+			return temp;
+		})
 
 		const embed = new MessageEmbed()
 			.setFooter(client.user.username, client.user.displayAvatarURL())
 			.setTimestamp()
 			.setTitle('**Help Menu**')
-			.addFields([
-				{ name: 'Test1', value: 'test1', inline: true },
-				{ name: 'Test2', value: 'test2', inline: true }
-			])
-			.setColor('GOLD');
+			.setColor('DARK_GREEN');
 
-		console.log(commandsNamesArr[0]);
-
-		for (var i = 0; i < commandsNamesArr.length; i++) {
-			const name = commandsNamesArr[i].toString();
-			const description = commandsDescriptionsArr[i].toString();
+		for (var i = 0; i < commandsNames.length; i++) {
+			const name = commandsNames[i].Name.toString();
+			const description = commandsNames[i].Description.toString();
 			console.log(name + ' : ' + description);
-			// embed.addField(`**${name}** : ${description} `, ', ', false);
+			embed.addField(`**${name}**`, description, false);
 		}
-
-		// for (let index = 0; index < commandsCollection.size; index++) {
-		// 	embed.addField(commandsCollection.data.name)
-
-		// }
 
 		await interaction.reply({
 			embeds: [ embed ]
 		});
 
-		// console.log(commandsCollectionNames[0], commandsCollectionDescriptions[0]);
 	}
 };
