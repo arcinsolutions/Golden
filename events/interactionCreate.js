61023d7739a173ff8ed505c487c8fcf61fd34550
= <=>
2021-10-27 13:49:13 +0200

    Disabled console log on use

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,7 @@
 module.exports = {
-	name: 'interactionCreate', 
-		execute(interaction) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} (${interaction.channel.id}) triggered an interaction (${interaction.commandName}).`);
-	},
-};
+    name: 'interactionCreate',
+    execute(interaction) {
+        //for Development 😉
+        // console.log(`${interaction.user.tag} in #${interaction.channel.name} (${interaction.channel.id}) triggered an interaction (${interaction.commandName}).`);
+    },
+}
