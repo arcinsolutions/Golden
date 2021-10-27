97f05aa2f88c0153dce2e024aae6eb83011feef7
= <=>
2021-10-27 13:49:43 +0200

    Added Bot latency

--- commands/information/ping.js
+++ commands/information/ping.js
@@ -1,32 +1,35 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { MessageEmbed } = require('discord.js')
+const path = require('path')
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!'),
-  async execute (interaction, client) {
-    const embed = new MessageEmbed()
-      .setFooter(client.user.username, client.user.displayAvatarURL())
-      .setTimestamp()
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
 
-    const sent = await interaction.reply({
-      embeds: [
-        embed.setDescription(
-          `**Pinging...**`
-        )
-        .setColor('RED')
-      ],
-      fetchReply: true
-    })
+    category: path.basename(__dirname),
+    async execute(interaction, client) {
+        const embed = new MessageEmbed()
+            .setFooter(client.user.username, client.user.displayAvatarURL())
+            .setTimestamp()
 
-    interaction.editReply({
-      embeds: [
-        embed.setDescription(
-          `**Roundtrip latency: ${sent.createdTimestamp -
-            interaction.createdTimestamp}ms**`
-        ).setColor('DARK_GREEN')
-      ]
-    })
-  }
+        const sent = await interaction.reply({
+            embeds: [embed.setDescription(`**Pinging...**`).setColor('RED')],
+            fetchReply: true,
+        })
+
+        interaction.editReply({
+            embeds: [
+                embed
+                    .setDescription(
+                        `**Roundtrip latency: ${
+                            sent.createdTimestamp - interaction.createdTimestamp
+                        }ms** , **Bot latency: ${Math.round(
+                            client.ws.ping
+                        )}ms**`
+                    )
+                    .setColor('DARK_GREEN'),
+            ],
+        })
+    },
 }
