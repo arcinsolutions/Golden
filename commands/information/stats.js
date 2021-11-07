254d3fc96b96750f57007e90b1169d41f862181d
= <=>
2021-11-07 15:05:39 +0100

    Prettier code

--- commands/information/stats.js
+++ commands/information/stats.js
@@ -18,26 +18,63 @@ module.exports = {
             fetchReply: true,
         })
 
-        let totalSeconds = (client.uptime / 1000);
-            let days = Math.floor(totalSeconds / 86400);
-            totalSeconds %= 86400;
-            let hours = Math.floor(totalSeconds / 3600);
-            totalSeconds %= 3600;
-            let minutes = Math.floor(totalSeconds / 60);
-            let seconds = Math.floor(totalSeconds % 60);
+        let totalSeconds = client.uptime / 1000
+        let days = Math.floor(totalSeconds / 86400)
+        totalSeconds %= 86400
+        let hours = Math.floor(totalSeconds / 3600)
+        totalSeconds %= 3600
+        let minutes = Math.floor(totalSeconds / 60)
+        let seconds = Math.floor(totalSeconds % 60)
 
-            const usage = process.cpuUsage();
+        const usage = process.cpuUsage()
 
         interaction.editReply({
             embeds: [
-                embed.setTitle(`**${client.user.username} - Stats**`)
+                embed
+                    .setTitle(`**${client.user.username} - Stats**`)
                     .setDescription('')
                     .addFields(
-                        { name: 'Uptime', value: `${days} day${((days===1)?'':'s')}, ${hours} hour${((hours===1)?'':'s')}, ${minutes} minute${((minutes===1)?'':'s')} & ${seconds} second${((seconds===1)?'':'s')}` },
-                        { name: `Ping`, value: `${sent.createdTimestamp - interaction.createdTimestamp}ms`, inline: true },
-                        { name: 'Discord API Ping', value: `${client.ws.ping}ms`, inline: true },
-                        { name: 'RAM usage', value: `${Math.round((process.memoryUsage().heapUsed / 1024 / 1024) * 100) / 100}MB\n`, inline: true },
-                        { name: 'CPU usage', value: `${process.cpuUsage(usage).user} %`, inline: true})
+                        {
+                            name: 'Uptime',
+                            value: `${days} day${
+                                days === 1 ? '' : 's'
+                            }, ${hours} hour${
+                                hours === 1 ? '' : 's'
+                            }, ${minutes} minute${
+                                minutes === 1 ? '' : 's'
+                            } & ${seconds} second${seconds === 1 ? '' : 's'}`,
+                        },
+                        {
+                            name: `Ping`,
+                            value: `${
+                                sent.createdTimestamp -
+                                interaction.createdTimestamp
+                            }ms`,
+                            inline: true,
+                        },
+                        {
+                            name: 'Discord API Ping',
+                            value: `${client.ws.ping}ms`,
+                            inline: true,
+                        },
+                        {
+                            name: 'RAM usage',
+                            value: `${
+                                Math.round(
+                                    (process.memoryUsage().heapUsed /
+                                        1024 /
+                                        1024) *
+                                        100
+                                ) / 100
+                            }MB\n`,
+                            inline: true,
+                        },
+                        {
+                            name: 'CPU usage',
+                            value: `${process.cpuUsage(usage).user} %`,
+                            inline: true,
+                        }
+                    )
                     .setColor('DARK_GREEN'),
             ],
         })
