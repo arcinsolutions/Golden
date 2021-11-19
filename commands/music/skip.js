c99e455b472006dc06c8ecfaceda2009ac287dc2
florian <fo@ostermaiers.de>
2021-11-19 11:25:37 +0100

    correct skip parameter

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { MessageEmbed } = require('discord.js')
 const path = require('path')
+const { skip } = require('../../functions/musicControl')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,38 +9,14 @@ module.exports = {
         .setDescription('Skip the current song!')
         .addIntegerOption((option) =>
         option
-            .setName('queuenumber')
-            .setDescription('Number of the song in the queue you want to skip to')
+            .setName('amount')
+            .setDescription('Amount of songs you want to skip')
             .setRequired(false)
     ),
 
     category: path.basename(__dirname),
     async execute(interaction, client) {
         await interaction.deferReply()
-
-        const guildId = interaction.guild.id;
-        const skipTo = interaction.options.getInteger('queuenumber')
-
-        const Queue = client.player.GetQueue(guildId)
-        if (!Queue || (Queue && !Queue.tracks[1])) {
-            const ErrorEmbed = {
-                title: 'Empty Queue',
-                description:
-                    "No Songs are playing in `Queue`\nOR, Next Track is not Present in Queue\nSongs can't be `Skipped`",
-            }
-            return void (await interaction.editReply({ embeds: [ErrorEmbed] }))
-        }
-        const success = Queue.skip(skipTo ?? undefined)
-        if (success) {
-            const ReturnEmbed = {
-                title: 'Songs has been Skipped',
-            }
-            return void (await interaction.editReply({ embeds: [ReturnEmbed] }))
-        }
-
-        const ErrorEmbed = {
-            title: "Songs can't be Skipped",
-        }
-        return void (await interaction.editReply({ embeds: [ErrorEmbed] }))
+        return void (await skip(interaction, client, interaction.options.getInteger('amount')))
     },
 }
