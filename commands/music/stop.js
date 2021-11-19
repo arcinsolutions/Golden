3a5d00d8f61583091b3310795dbdb5c7b0be1f35
florian <fo@ostermaiers.de>
2021-11-19 11:25:53 +0100

    made stop command more compact

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { MessageEmbed } = require('discord.js')
 const path = require('path')
+const { stop } = require('../../functions/musicControl')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,29 +10,7 @@ module.exports = {
 
     category: path.basename(__dirname),
     async execute(interaction, client) {
-        await interaction.deferReply()
-
-        const guildId = interaction.guild.id;
-
-        const Queue = client.player.GetQueue(guildId);
-        if (!Queue || (Queue && !Queue.current)) {
-          const ErrorEmbed = {
-            title: 'Empty Queue',
-            description: 'No Songs are playing in `Queue`\nSongs can\'t be `Stopped`',
-          };
-          return void (await interaction.editReply({ embeds: [ErrorEmbed] }));
-        }
-        const success = Queue.stop();
-        if (success) {
-          const ReturnEmbed = {
-            title: 'Songs has been Stopped',
-          };
-          return void (await interaction.editReply({ embeds: [ReturnEmbed] }));
-        }
-        const ErrorEmbed = {
-          title: 'Songs can\'t be Stopped',
-        };
-        return void (await interaction.editReply({ embeds: [ErrorEmbed] }));
-
+        await interaction.deferReply();
+        return void (await stop(interaction, client));
     },
 }
