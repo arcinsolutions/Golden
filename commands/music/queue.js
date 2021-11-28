b27e8a09ee42d4030b047f6690528486a0eb7778
Botis <barnekiel@gmail.com>
2021-11-28 21:17:06 +0100

    remake Queue, its pretty clean now :)

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -8,8 +8,8 @@ module.exports = {
         .setDescription('let you see the current Queue')
         .addIntegerOption((option) =>
             option
-                .setName('amount')
-                .setDescription('amountt')
+                .setName('page')
+                .setDescription('the Page of the Queue, you want to see')
                 .setRequired(false)
         ),
 
@@ -17,49 +17,55 @@ module.exports = {
     async execute(interaction, client) {
         await interaction.deferReply()
 
-        const guildId = interaction.guild.id;
-        const amount = interaction.options.getInteger('amount') // KA WAS DAS MACHT
+        const embed = new MessageEmbed()
+            .setTitle('**🎶 | Queue:**')
+            .setFooter(client.user.username, client.user.displayAvatarURL())
+            .setTimestamp()
 
-        const Queue = client.player.GetQueue(guildId)
-        if (!Queue || (Queue && !Queue.current)) {
-            const ErrorEmbed = {
-                title: 'Empty Queue',
-                description:
-                    "No Songs are playing in `Queue`\nSong can't be `Fetched from Queue`",
-            }
-            return void (await interaction.editReply({ embeds: [ErrorEmbed] }))
-        }
-        var Index =
-        amount &&
-            Number(amount) &&
-            Number(amount) < Queue.tracks.length &&
-            Number(amount) > 0
-                ? Number(amount)
-                : 0
+        const guildId = interaction.guild.id
+        let page = interaction.options.getInteger('page')
+        const queue = client.player.GetQueue(guildId)
+        const currentTrack = queue.current
 
-        var StringArrays = Queue.tracks.map(
-            (track, index) =>
-                `**Track Index :** \`${index}\`\n**Track ID :** \`${track.Id}\`\n**Name :** \`${track.title}\`\n**Author :** \`${track.channelId}\`\n**Duration :** \`${track.human_duration}\`\n**URl :** [Track Url](${track.url})\n`
-        )
-        StringArrays = StringArrays.slice(Index, Index + 5)
-        StringArrays = StringArrays.filter(Boolean)
-        if (Queue.tracks.length > StringArrays.length) {
-            StringArrays.push(
-                `More \`${Number(
-                    Queue.tracks.length - (5 + Index)
-                )}+\` Tracks are Present in Queue`
-            )
-        }
-        const ReturnEmbed = {
-            title: 'Current Queue Stats',
-            description: `__**Current ${Index + 1}/${
-                Queue.tracks.length
-            } Tracks Data**__\n\n${StringArrays.join('\n')}`,
-            field: {
-                title: `Queue Progress Bar`,
-                value: Queue.createProgressBar('queue'),
-            },
-        }
-        return void (await interaction.editReply({ embeds: [ReturnEmbed] }))
+        if (!queue || !queue.playing)
+            return void interaction.editReply({
+                embeds: [
+                    embed
+                        .setDescription('**❌ | No music is being played!**')
+                        .setColor('DARK_RED'),
+                ],
+            })
+
+        if (page == null) page = 1
+        const pageStart = 10 * (page - 1)
+        const pageEnd = pageStart + 10
+        const tracks = queue.tracks
+            .slice(pageStart, pageEnd)
+            .map((track, i) => {
+                let pos = i + pageStart + 1
+                if (pos == 1)
+                    return `\`Now Playing.\` ** | [${track.title} by ${track.channelId}](${track.url})**`
+                else
+                    return `\`${i + pageStart + 0}.\` ** | [${track.title} by ${
+                        track.channelId
+                    }](${track.url})**`
+
+            })
+
+        return interaction.editReply({
+            embeds: [
+                embed
+                    .setDescription(
+                        `${tracks.join('\n')}${
+                            queue.tracks.length > pageEnd
+                                ? `\nand... \`${
+                                      queue.tracks.length - pageEnd
+                                  }\` more track(s)`
+                                : ''
+                        }`
+                    )
+                    .setColor('DARK_GREEN'),
+            ],
+        })
     },
 }
