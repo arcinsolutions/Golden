5103bb254c5bb8d796ea51a3df6ded02ecbb1f6e
Botis <barnekiel@gmail.com>
2021-11-30 22:26:44 +0100

    half done new Rework of the Queue command

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,40 +1,19 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js')
 const path = require('path')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('queue')
-        .setDescription('let you see the current Queue')
-        .addIntegerOption((option) =>
-            option
-                .setName('page')
-                .setDescription('the Page of the Queue, you want to see')
-                .setRequired(false)
-        ),
+        .setDescription('let you see the current Queue'),
 
     category: path.basename(__dirname),
     async execute(interaction, client) {
         await interaction.deferReply()
-
-        const embed = new MessageEmbed()
-            .setTitle('**🎶 | Queue:**')
-            .setFooter(client.user.username, client.user.displayAvatarURL())
-            .setTimestamp()
+        let page = 1
 
         const guildId = interaction.guild.id
-        let page = interaction.options.getInteger('page')
         const queue = client.player.GetQueue(guildId)
-        const currentTrack = queue.current
-
-        if (!queue || !queue.playing)
-            return void interaction.editReply({
-                embeds: [
-                    embed
-                        .setDescription('**❌ | No music is being played!**')
-                        .setColor('DARK_RED'),
-                ],
-            })
 
         if (page == null) page = 1
         const pageStart = 10 * (page - 1)
@@ -49,23 +28,101 @@ module.exports = {
                     return `\`${i + pageStart + 0}.\` ** | [${track.title} by ${
                         track.channelId
                     }](${track.url})**`
-
             })
 
-        return interaction.editReply({
-            embeds: [
-                embed
-                    .setDescription(
-                        `${tracks.join('\n')}${
-                            queue.tracks.length > pageEnd
-                                ? `\nand... \`${
-                                      queue.tracks.length - pageEnd
-                                  }\` more track(s)`
-                                : ''
-                        }`
-                    )
-                    .setColor('DARK_GREEN'),
-            ],
+        await interaction.editReply({
+            embeds: [this.setEmbed(queue, page)],
         })
+
+        const filter = (page) => (page.customId === 'previous' || page.customId === 'next') && page.user.id === interaction.user.id
+
+        const collector = interaction.channel.createMessageComponentCollector({
+            filter,
+        })
+
+        collector.on('collect', async (button) => {
+            if (button.customId === 'previous') {
+                await (page-=1)
+                await button.update({
+                    embeds: [this.setEmbed(client, categories[page])],
+                    components: [this.setButton(categories, page)],
+                })
+            }
+            if (button.customId === 'next') {
+                await (i+=1)
+                await button.update({
+                    embeds: [this.setEmbed(client, categories[page])],
+                    components: [this.setButton(categories, page)],
+                })
+            }
+        })
+    },
+
+    setEmbed: function (client, page) {
+        const queue = client.player.GetQueue(guildId)
+        const embed = new MessageEmbed()
+            .setTitle('**🎶 | Queue:**')
+            .setFooter(client.user.username, client.user.displayAvatarURL())
+            .setTimestamp()
+            .setColor('DARK_GREEN')
+
+        if (!queue || !queue.playing)
+            return embed
+                .setDescription('**❌ | No music is being played!**')
+                .setColor('DARK_RED')
+
+        if (page == null) page = 1
+        const pageStart = 10 * (page - 1)
+        const pageEnd = pageStart + 10
+        const tracks = queue.tracks
+            .slice(pageStart, pageEnd)
+            .map((track, i) => {
+                let pos = i + pageStart + 1
+                if (pos == 1)
+                    return `\`Now Playing.\` ** | [${track.title} by ${track.channelId}](${track.url})**`
+                else
+                    return `\`${i + pageStart + 0}.\` ** | [${track.title} by ${
+                        track.channelId
+                    }](${track.url})**`
+            })
+
+        embed
+            .setDescription(
+                `${tracks.join('\n')}${
+                    queue.tracks.length > pageEnd
+                        ? `\nand... \`${
+                              queue.tracks.length - pageEnd
+                          }\` more track(s)`
+                        : ''
+                }`
+            )
+            .setColor('DARK_GREEN')
+
+        return embed
+    },
+
+    setButton: function (queue, page) {
+        let previous = false
+        if (page == 0) previous = true
+        else previous = false
+
+        let next = true
+        if (page == (queue.length/10) - 1) next = true
+        else next = false
+
+        const buttons = new MessageActionRow().addComponents(
+            new MessageButton()
+                .setCustomId('previous')
+                .setLabel('Previous')
+                .setStyle('PRIMARY')
+                .setDisabled(previous),
+            new MessageButton()
+                .setCustomId('next')
+                .setLabel('Next')
+                .setStyle('PRIMARY')
+                .setDisabled(next)
+        )
+
+        return buttons
     },
 }
