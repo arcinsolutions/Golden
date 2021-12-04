adeec80c3806a05d49b46b6e03838105f2c62313
Botis <barnekiel@gmail.com>
2021-12-04 14:01:06 +0100

    Finished Queue rework

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -9,56 +9,48 @@ module.exports = {
 
     category: path.basename(__dirname),
     async execute(interaction, client) {
-        await interaction.deferReply()
-        let page = 1
+        await interaction.deferReply({ ephemeral: true})
+        let page = 0
 
-        const guildId = interaction.guild.id
-        const queue = client.player.GetQueue(guildId)
-
-        if (page == null) page = 1
-        const pageStart = 10 * (page - 1)
-        const pageEnd = pageStart + 10
-        const tracks = queue.tracks
-            .slice(pageStart, pageEnd)
-            .map((track, i) => {
-                let pos = i + pageStart + 1
-                if (pos == 1)
-                    return `\`Now Playing.\` ** | [${track.title} by ${track.channelId}](${track.url})**`
-                else
-                    return `\`${i + pageStart + 0}.\` ** | [${track.title} by ${
-                        track.channelId
-                    }](${track.url})**`
-            })
+        const guildId = await interaction.guild.id
 
         await interaction.editReply({
-            embeds: [this.setEmbed(queue, page)],
+            embeds: [this.QueSetEmbed(client, guildId, page)],
+            components: [this.QueSetButtons(client, guildId, page)],
         })
 
-        const filter = (page) => (page.customId === 'previous' || page.customId === 'next') && page.user.id === interaction.user.id
+
+        /** ++ Button Collector ++ */
+        const filter = (button) =>
+            (button.customId === 'quePrevious' || button.customId === 'queNext') &&
+            button.user.id === interaction.user.id && interaction.id === button.message.interaction.id
 
         const collector = interaction.channel.createMessageComponentCollector({
             filter,
         })
-
         collector.on('collect', async (button) => {
-            if (button.customId === 'previous') {
-                await (page-=1)
+            switch (button.customId) {
+            case 'quePrevious':
+                await (page --)
                 await button.update({
-                    embeds: [this.setEmbed(client, categories[page])],
-                    components: [this.setButton(categories, page)],
+                    embeds: [this.QueSetEmbed(client, guildId, page)],
+                    components: [this.QueSetButtons(client, guildId, page)],
                 })
-            }
-            if (button.customId === 'next') {
-                await (i+=1)
+                break
+
+            case 'queNext':
+                await (page ++)
                 await button.update({
-                    embeds: [this.setEmbed(client, categories[page])],
-                    components: [this.setButton(categories, page)],
+                    embeds: [this.QueSetEmbed(client, guildId, page)],
+                    components: [this.QueSetButtons(client, guildId, page)],
                 })
+                break
             }
         })
+        /** -- Button Collector -- */
     },
 
-    setEmbed: function (client, page) {
+    QueSetEmbed: function (client, guildId, page) {
         const queue = client.player.GetQueue(guildId)
         const embed = new MessageEmbed()
             .setTitle('**🎶 | Queue:**')
@@ -71,8 +63,9 @@ module.exports = {
                 .setDescription('**❌ | No music is being played!**')
                 .setColor('DARK_RED')
 
-        if (page == null) page = 1
-        const pageStart = 10 * (page - 1)
+        if (page <= 0) page = 0
+        
+        const pageStart = 10 * (page)
         const pageEnd = pageStart + 10
         const tracks = queue.tracks
             .slice(pageStart, pageEnd)
@@ -101,23 +94,37 @@ module.exports = {
         return embed
     },
 
-    setButton: function (queue, page) {
+    QueSetButtons: function (client, guildId, page) {
+        const queue = client.player.GetQueue(guildId)
+
         let previous = false
-        if (page == 0) previous = true
+        if (page <= 0.1) previous = true
         else previous = false
 
+        if (!queue || !queue.playing) return
+
         let next = true
-        if (page == (queue.length/10) - 1) next = true
+
+        // const currPage = (page/10)+1
+        const currPage = page++
+        if (currPage >= (Math.floor((queue.tracks.length-1)/10))) next = true
         else next = false
+
+        const pages = `Page: ${currPage+1} / ${Math.floor((queue.tracks.length-1)/10)+1}`
 
         const buttons = new MessageActionRow().addComponents(
             new MessageButton()
-                .setCustomId('previous')
+                .setCustomId('quePrevious')
                 .setLabel('Previous')
                 .setStyle('PRIMARY')
                 .setDisabled(previous),
             new MessageButton()
-                .setCustomId('next')
+                .setCustomId('quePages')
+                .setLabel(pages)
+                .setStyle('SUCCESS')
+                .setDisabled(true),
+            new MessageButton()
+                .setCustomId('queNext')
                 .setLabel('Next')
                 .setStyle('PRIMARY')
                 .setDisabled(next)
