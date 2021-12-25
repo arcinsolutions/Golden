721dbfa33836e2edbdfe390cf5915be4a820980c
Anweisung <xd98474@gmail.com>
2021-12-25 17:55:23 +0100

    cast env variable

--- src/golden.js
+++ src/golden.js
@@ -26,7 +26,7 @@ client.manager = new Manager({
       port: Number(process.env.PORT),
       password: process.env.PASSWORD,
       retryDelay: 5000,
-      secure: process.env.SECURE
+      secure: Boolean(process.env.SECURE)
     },
   ],
   plugins: [
