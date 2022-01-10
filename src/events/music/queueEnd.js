dd119b14cf4ee7d2da0fef09a5bd736f06355fa3
Barne Bellin <barnekiel@gmail.com>
2022-01-10 14:21:30 +0100

    changed to Random Song from Mix

--- src/events/music/queueEnd.js
+++ src/events/music/queueEnd.js
@@ -1,11 +1,13 @@
 const { resetChannel } = require("../../modules/channelModule/channelModule");
 
-module.exports = async (client, player) => {
-  if (player.get('autoplay')) {
+module.exports = async (client, player) =>
+{
+  if (player.get('autoplay'))
+  {
     const previoustrack = player.get("previoustrack");
     const mixURL = `https://www.youtube.com/watch?v=${previoustrack.identifier}&list=RD${previoustrack.identifier}`;
     const response = await player.search(mixURL, previoustrack.requester);
-    return player.play(response.tracks[1])
+    return player.play(tracks[Math.floor(Math.random() * response.tracks.length)]);
   }
 
   const guild = await client.guilds.fetch(player.guild);
