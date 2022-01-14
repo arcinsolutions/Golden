72115eccb0d46ac14cd2dae48087ae11cfef4bb6
Botis <barnekiel@gmail.com>
2022-01-14 23:45:57 +0100

    format

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -6,15 +6,16 @@ module.exports = {
     .setName("stop")
     .setDescription("Stop playing music"),
 
-  async execute(interaction, client) {
+  async execute(interaction, client)
+  {
     const player = interaction.client.manager.get(interaction.guild.id);
     if (!player) return replyInteractionEmbed(interaction, '', 'Play a track before using this command.', 'DARK_RED');
 
     const { channel } = interaction.member.voice;
-    
+
     if (!channel) return replyInteraction(interaction, '', 'Join a voice channel first.', 'DARK_RED');
     if (channel.id !== player.voiceChannel) return replyInteractionEmbed(interaction, '', 'I\'ve to be in the same voice channel with you for requesting tracks.', 'DARK_RED');
-    
+
     player.destroy();
     resetChannel(interaction.guild);
     return replyInteractionEmbed(interaction, '', 'Stopped playing and cleared queue.', 'DARK_GREEN');
