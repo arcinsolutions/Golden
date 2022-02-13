a465bc11c0c4261a027c40eb3bba64e68ac926a5
Botis <barnekiel@gmail.com>
2022-02-13 15:27:28 +0100

    finished Development for /skip command

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -1,46 +1,110 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { replyInteractionEmbed } = require("../../modules/channelModule/channelModule");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const {
+	replyInteractionEmbed,
+} = require('../../modules/channelModule/channelModule');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("skip")
-    .setDescription("Skip current song")
-    .addIntegerOption((option) => option.setName('amount').setDescription('the amount of songs to be skipped'))
-    .addIntegerOption((option) => option.setName('queue_number').setDescription('the Number of the song in the Queue to which you want to Skip')),
+	data: new SlashCommandBuilder()
+		.setName('skip')
+		.setDescription('Skip current song')
+		.addIntegerOption((option) =>
+			option
+				.setName('amount')
+				.setDescription('the amount of songs to be skipped')
+		)
+		.addIntegerOption((option) =>
+			option
+				.setName('queue_number')
+				.setDescription(
+					'the Number of the song in the Queue to which you want to Skip'
+				)
+		),
 
-  async execute(interaction, client)
-  {
-    const amount = interaction.options.getString('amount');
-    const queue_number = interaction.options.getString('queue_number');
-    const player = interaction.client.manager.get(interaction.guild.id);
-    if (!player) return replyInteractionEmbed(interaction, '', 'Play a track before using this command.', 'DARK_RED');
+	async execute(interaction, client) {
+		const amount = interaction.options.getInteger('amount');
+		const queue_number = interaction.options.getInteger('queue_number');
 
-    const { channel } = interaction.member.voice;
-    if (!channel) return replyInteractionEmbed(interaction, '', 'Join a voice channel first.', 'DARK_RED');
-    if (channel.id !== player.voiceChannel) return replyInteractionEmbed(interaction, '', 'I\'ve to be in the same voice channel with you for requesting tracks.', 'DARK_RED');
+		const player = interaction.client.manager.get(interaction.guild.id);
+		const queue = player.queue;
 
-    if (!player.queue.current) return replyInteractionEmbed(interaction, '', 'There is no music playing.', 'DARK_RED');
+		if (!player)
+			return replyInteractionEmbed(
+				interaction,
+				'',
+				'Play a track before using this command.',
+				'DARK_RED'
+			);
 
-    const { title } = player.queue.current;
+		const { channel } = interaction.member.voice;
+		if (!channel)
+			return replyInteractionEmbed(
+				interaction,
+				'',
+				'Join a voice channel first.',
+				'DARK_RED'
+			);
+		if (channel.id !== player.voiceChannel)
+			return replyInteractionEmbed(
+				interaction,
+				'',
+				"I've to be in the same voice channel with you for requesting tracks.",
+				'DARK_RED'
+			);
 
-    if (amount != undefined) 
-    {
-      if (player.queue.totalSize > amount)
-        return replyInteractionEmbed(interaction, '', `Queue isnt that Big ;).`, 'DARK_RED');
-      for (let i = 0; i <= amount; i++)
-        await player.play();
-      return replyInteractionEmbed(interaction, '', `Songs was skipped.`, 'DARK_GREEN');
-    }
-    else if (queue_number != undefined)
-    {
-      if (player.queue.size > queue_number)
-        return replyInteractionEmbed(interaction, '', `Queue isnt that Big ;).`, 'DARK_RED');
-      for (let i = 0; i <= queue_number; i++)
-        await player.play();
-      return replyInteractionEmbed(interaction, '', `Songs was skipped.`, 'DARK_GREEN');
-    }
+		if (!player.queue.current)
+			return replyInteractionEmbed(
+				interaction,
+				'',
+				'There is no music playing.',
+				'DARK_RED'
+			);
 
-    player.play();
-    return replyInteractionEmbed(interaction, '', `${title} was skipped.`, 'DARK_GREEN');
-  },
+		const { title } = player.queue.current;
+
+		if (amount != undefined) {
+			if (player.queue.totalSize < amount)
+				return replyInteractionEmbed(
+					interaction,
+					'',
+					`Queue is not that Big, please use a smaller Number.`,
+					'DARK_RED'
+				);
+
+			await queue.remove(0, amount);
+			await player.stop();
+
+			return replyInteractionEmbed(
+				interaction,
+				'Songs were skipped',
+				`Now Playing ${queue.current.title}`,
+				'DARK_GREEN'
+			);
+		} else if (queue_number != undefined) {
+			if (player.queue.totalSize < queue_number)
+				return replyInteractionEmbed(
+					interaction,
+					'',
+					`Queue is not that Big, please use a smaller Number.`,
+					'DARK_RED'
+				);
+
+			await queue.remove(0, queue_number - 1);
+			await player.stop();
+
+			return replyInteractionEmbed(
+				interaction,
+				'Songs were skipped',
+				`Now Playing ${queue.current.title}`,
+				'DARK_GREEN'
+			);
+		}
+
+		player.stop();
+		return replyInteractionEmbed(
+			interaction,
+			'',
+			`${title} was skipped.`,
+			'DARK_GREEN'
+		);
+	},
 };
